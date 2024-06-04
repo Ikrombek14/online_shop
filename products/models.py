71acd213409c9bf42760596b57c4782562eea103
zarnigordekhonova <zarnigor1008@gmail.com>
2024-06-04 20:20:52 +0500

    zarnigor

--- products/models.py
+++ products/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
 # Create your models here.
+
+class ProductsCategory(models.Model):
+    name = models.CharField(max_length=64)
+
+    class Meta:
+        db_table = 'products_category'
+
+    def __str__(self):
+        return self.name
+
+
+class ProductsMake(models.Model):
+    name = models.CharField(max_length=64)
+
+    class Meta:
+        db_table = 'products_make'
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=128, blank=True, null=True)
+    description = models.TextField()
+    category = models.ForeignKey(ProductsCategory, on_delete=models.DO_NOTHING)
+    characteristics = models.TextField(blank=True, null=True)
+    price = models.IntegerField()
+    make = models.ForeignKey(ProductsMake, on_delete=models.DO_NOTHING)
+    image = models.ImageField(upload_to='products_images/', blank=True, null=True, default='not_available.png')
+
+    class Meta:
+        db_table = 'products'
+
+    def __str__(self):
+        return f'{self.name} - {self.category.name}'
